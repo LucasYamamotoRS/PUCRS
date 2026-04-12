8b260700bb7acf72f2a90929d8c221bfffa43b9f
Lucas Yamamoto <Lucas.Yamamoto@live.com>
2026-04-12 14:29:39 -0300

    Substitua o laço de repetição 'for' por 'while'.

--- Logica/Fase1/Logica_Fase1.py
+++ Logica/Fase1/Logica_Fase1.py
@@ -14,44 +14,57 @@ Após a coleta dos dados, o programa calcula e exibe os seguintes resultados:
 
 """
 
-#Dicionário/Indice que associa o número ao nome do respectivo mês.
-mesesindice = {1: 'Janeiro', 2: 'Fevereiro', 3: 'Marco', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
+# intervalo de temperaturas devera ser informado em Celcius, entre -60 graus a +50 graus.
+mesesindice = {
+    1: 'Janeiro', 2: 'Fevereiro', 3: 'Marco', 4: 'Abril', 5: 'Maio', 6: 'Junho',
+    7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
+}
 
-#Variaveis utilizadas:
-mediamaximaanual = 0
+tempmaxima = 0
+mediaanual = 0
+mesescaldante = 0
 qtdmesesescaldantes = 0
-mesmaisescaldante = -61
-mesmenosquente = 51
+mesmenosquente = 0
 mesquente = ''
 mesfrio = ''
+cont = 1
 
-#Primeiro laço: Refere-se aos 12 meses do ano, dos quais serão coletadas as temperaturas máximas de cada mês inseridas pelo usuário.
-for meses in range (1, 13):
-    #Segundo laço: Usado para verificação dos dados inseridos pelo usuário. Caso estejam fora dos parâmetros, o usuário podera reinseri-lo novamente.
-    #Caso estejam dentro dos parâmetros, os dados são coletados e o laço é interrompido pelo comando "break".
-    for tentativa in range (100):
-        tempmaxima = float(input('Digite a temperatura do mes {}: '.format(meses)))
-        #Verifica se a temperatura está dentro do intervalo (-60°C e +50°C).
-        if tempmaxima < 51 and tempmaxima > -61:
-            mediamaximaanual += tempmaxima
-            #Verifica e contabiliza (caso verdadeira) se a temperatura é superior ao parâmetro (> 33°C).
-            if tempmaxima > 33:
-                qtdmesesescaldantes += 1
-            #Armazena o mês mais quente e compara com o atual. Caso a atual seja maior (mais quente), substitui e armazena os novos dados.
-            if tempmaxima > mesmaisescaldante:
-                mesmaisescaldante = tempmaxima
-                mesquente = mesesindice.get(meses)
-            #Armazena o mês menos quente e compara com o atual. Caso o atual seja menor (menos quente), substitui e armazena os novos dados.
-            if tempmaxima < mesmenosquente:
-                mesmenosquente = tempmaxima
-                mesfrio = mesesindice.get(meses)
-            break
-        #Mensagem informando ao usuário que os dados inseridos estão fora dos parâmetros.
-        else:
-            print('Temperatura invalida, informe um valor entre -60°C e 50°C.')
+# Laço que percorre os meses do dicionário utilizando a variável cont.
+while cont <= 12:
+    tempmaxima = float(input(f'Digite a temperatura referente ao mes de {mesesindice[cont]}: '))
+    if tempmaxima < 51 and tempmaxima > -61:
+        mediaanual += tempmaxima
 
-#Exibição dos resultados.
-print('A media anual das temperaturas maximas: {:.1f}°C'.format(mediamaximaanual/12))
-print('Total de meses escaldantes (temperaturas > 33°C): {}'.format(qtdmesesescaldantes))
-print('O mes mais escaldante do ano foi: {}'.format(mesquente))
-print('O mes menos quente do ano foi: {}'.format(mesfrio))
+        # Verifica e contabiliza (caso verdadeiro) se a temperatura é superior ao parâmetro (> 33°C).
+        if tempmaxima > 33:
+            qtdmesesescaldantes += 1
+
+        # Se for a primeira iteração (cont == 1), os dados serão armazenados em suas respectivas variáveis.
+        if cont == 1:
+            mesescaldante = tempmaxima
+            mesquente = mesesindice[cont]
+            mesmenosquente = tempmaxima
+            mesfrio = mesesindice[cont]
+
+        # Armazena o mês mais quente e compara com o atual. Caso a atual seja maior (mais quente), substitui e armazena os novos dados.
+        elif tempmaxima > mesescaldante:
+            mesescaldante = tempmaxima
+            mesquente = mesesindice[cont]
+
+        # Armazena o mês menos quente e compara com o atual. Caso o atual seja menor (menos quente), substitui e armazena os novos dados.
+        elif tempmaxima < mesmenosquente:
+            mesmenosquente = tempmaxima
+            mesfrio = mesesindice[cont]
+
+        cont += 1
+
+    else:
+        # Mensagem informando ao usuário que os dados inseridos estão fora dos parâmetros.
+        print('Temperatura invalida, informe um valor entre -60°C e 50°C.')
+
+
+#Exibição dos resultados
+print(f'A media anual das temperaturas maximas: {mediaanual/12:.1f}°C')
+print(f'Total de meses escaldantes (temperaturas > 33°C): {qtdmesesescaldantes}')
+print(f'O mes mais escaldante do ano foi: {mesquente}')
+print(f'O mes menos quente do ano foi: {mesfrio}')
